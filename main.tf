147f4afb4982e94c668ff743158fbb25bef03fe0
nrayavaram <91076970+nrayavaram@users.noreply.github.com>
2021-11-29 11:32:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,23 +3,84 @@ provider "google" {
  project = "airline1-sabre-wolverine"
 }
 
+/*
+google_project_iam_policy
+google_project_iam_binding
+google_project_iam_member
+google_organization_iam_policy
+google_organization_iam_binding
+google_organization_iam_member
+
+*/
+
+
 resource "google_project_iam_policy" "project" {
   project     = "airline1-sabre-wolverine"
-  policy_data = "${data.google_iam_policy.admin.policy_data}"
+  policy_data = data.google_iam_policy.admin-project.policy_data
+}
+
+data "google_iam_policy" "admin-project" {
+  binding {
+    role = "roles/editor"
+
+    members = [
+      #"user:romil@example.com",
+      "group:rajeevgroup@googlegroup.com",
+      "serviceAccount:407536364561-compute@developer.gserviceaccount.com"
+    ]
+  }
+}
+
+resource "google_project_iam_binding" "project-binding" {
+  project = "airline1-sabre-wolverine"
+  role    = "roles/editor"
+
+  members = [
+    #"user:jane@example.com",
+    "group:rajeevgroup@googlegroup.com",
+    "serviceAccount:407536364561-compute@developer.gserviceaccount.com"
+  ]
+}
+
+resource "google_project_iam_member" "project-iam-member" {
+  project = "airline1-sabre-wolverine"
+  role    = "roles/editor"
+  #member  = "user:ravi@example.com"
+  member = "group:rajeevgroup@googlegroup.com"
+  
+}
+
+resource "google_organization_iam_policy" "organization" {
+  org_id      = "airline1"
+  policy_data = data.google_iam_policy.admin.policy_data
 }
 
 data "google_iam_policy" "admin" {
   binding {
-    role = "roles/compute.admin"
+    role = "roles/editor"
 
     members = [
-      "user:nrayavaram@google.com",
+      #"user:mahesh@example.com",
+      "group:rajeevgroup@googlegroup.com",
+      "serviceAccount:407536364561-compute@developer.gserviceaccount.com"
     ]
-
-    condition {
-      title       = "expires_after_2019_12_31"
-      description = "Expiring at midnight of 2019-12-31"
-      expression  = "request.time < timestamp(\"2020-01-01T00:00:00Z\")"
-    }
   }
 }
+
+resource "google_organization_iam_binding" "organization-binding" {
+  org_id      = "airline1"
+  role    = "roles/editor"
+
+  members = [
+    #"user:jane@example.com",
+    "serviceAccount:407536364561-compute@developer.gserviceaccount.com"
+  ]
+}
+
+resource "google_organization_iam_member" "organization-member" {
+  org_id      = "airline1"  
+  role    = "roles/editor"
+  #member  = "user:rajeev@example.com"
+  member = "group:rajeevgroup@googlegroup.com"
+}
+
